3dee517b6b8dd87fa71b3be517f0ba555b41ec76
Sajan-giri <166353829+Sajan-giri@users.noreply.github.com>
2026-07-12 21:03:47 +1000

    Load tracker zoom controls last

--- frontend/app/tracker/layout.tsx
+++ frontend/app/tracker/layout.tsx
@@ -5,15 +5,16 @@ import "./tracker-compact.css";
 import "./tracker-responsive.css";
 import "./tracker-scroll-controls.css";
 import "./tracker-final.css";
+import "./tracker-zoom-final.css";
 import TrackerBulkApprove from "./TrackerBulkApprove";
-import TrackerHorizontalScroll from "./TrackerHorizontalScroll";
+import TrackerSheetZoom from "./TrackerSheetZoom";
 
 export default function TrackerLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <TrackerBulkApprove />
-      <TrackerHorizontalScroll />
+      <TrackerSheetZoom />
     </>
   );
 }
