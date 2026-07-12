248020c6fd38e65e4629c20160d454dde5f2d14b
Sajan-giri <166353829+Sajan-giri@users.noreply.github.com>
2026-07-12 20:34:47 +1000

    Import final tracker cascade

--- frontend/app/tracker/layout.tsx
+++ frontend/app/tracker/layout.tsx
@@ -4,6 +4,7 @@ import "./tracker-polish.css";
 import "./tracker-compact.css";
 import "./tracker-responsive.css";
 import "./tracker-scroll-controls.css";
+import "./tracker-final.css";
 import TrackerBulkApprove from "./TrackerBulkApprove";
 import TrackerHorizontalScroll from "./TrackerHorizontalScroll";
 
