bd209eed84c09e18778345b8e416527f831477a2
Sajan-giri <166353829+Sajan-giri@users.noreply.github.com>
2026-07-12 18:55:24 +1000

    Load responsive tracker overrides

--- frontend/app/tracker/layout.tsx
+++ frontend/app/tracker/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import "./tracker-sheet.css";
 import "./tracker-polish.css";
 import "./tracker-compact.css";
+import "./tracker-responsive.css";
 import TrackerBulkApprove from "./TrackerBulkApprove";
 
 export default function TrackerLayout({ children }: { children: ReactNode }) {
